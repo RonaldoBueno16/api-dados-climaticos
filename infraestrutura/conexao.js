6ff67cd9a4029b49e597f84536940c13afa526d2
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-08-28 18:51:31 -0300

    Update conexao.js

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const conexao = mysql.createConnection({
-    host: 'localhost',
-    port: '3307',
-    user: 'root',
-    password: 'admin',
-    database: 'espdb'
+    host: 'ec2-3-89-0-52.compute-1.amazonaws.com',
+    port: '5432',
+    user: 'huqfhjrbdjoftd',
+    password: 'a75851533a05e9c43dfe0bdded4962e8ffb770eeb9a175c83d8b8d5ff7e64d98',
+    database: 'd64kvt94usu3co'
 })
 
 module.exports = conexao;
