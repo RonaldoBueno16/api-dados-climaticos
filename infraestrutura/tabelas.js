43f41c9d9e7e89431ef447da6842614978e46c25
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-08-28 16:11:05 -0300

    Alteração da tabela

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -6,7 +6,7 @@ class tabelas {
     }
 
     estruturarDB() {
-        const sql = "CREATE TABLE IF NOT EXISTS `dados_climaticos` ( `id` INT(10) NOT NULL AUTO_INCREMENT, `umidade` INT(10) NOT NULL DEFAULT '0', `temperatura` INT(10) NOT NULL DEFAULT '0', `pressao_atmosferica` INT(10) NOT NULL DEFAULT '0', `esta_chovendo` INT(10) NOT NULL DEFAULT '0', PRIMARY KEY (`id`) USING BTREE);";
+        const sql = "CREATE TABLE IF NOT EXISTS `dados_climaticos` (`id` INT(10) NOT NULL AUTO_INCREMENT,`esp_id` VARCHAR(50) NOT NULL COLLATE 'latin1_bin',`datadoregistro` DATETIME NULL DEFAULT NULL,`umidade` INT(10) NOT NULL DEFAULT '0',`temperatura` INT(10) NOT NULL DEFAULT '0',`pressao_atmosferica` INT(10) NOT NULL DEFAULT '0',`esta_chovendo` INT(10) NOT NULL DEFAULT '0',`latitude` FLOAT NOT NULL DEFAULT '0',`longitude` FLOAT NOT NULL DEFAULT '0',PRIMARY KEY (`id`));";        
         
         this.conexao.query(sql, (erro) => {
             if(erro) {
