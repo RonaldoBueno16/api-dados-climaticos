30574ebe33f6eae873d8ef889d5a80076ac7a4dd
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-08-28 21:14:36 -0300

    Update conexao.js

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql')
 
-const conexao = mysql.createConnection({
+const conexao = mysql.createPool({
     host: 'us-cdbr-east-04.cleardb.com',
     port: '3306',
     user: 'b06f325f729dc2',
