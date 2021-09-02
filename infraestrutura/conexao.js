ce58dc17593bb7d16a6695926abd030d91075128
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-09-01 23:55:56 -0300

    Atualização na conexão

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql')
 
 const conexao = mysql.createPool({
-    /*host: 'us-cdbr-east-04.cleardb.com',
+    host: 'us-cdbr-east-04.cleardb.com',
     port: '3306',
     user: 'b06f325f729dc2',
     password: '77211caf',
-    database: 'heroku_9981c230de2b387'*/
+    database: 'heroku_9981c230de2b387'
 
-    host: '127.0.0.1',
+    /*host: '127.0.0.1',
     port: '3306',
     user: 'root',
     password: '',
-    database: 'espdb'
+    database: 'espdb'*/
 })
 
 module.exports = conexao;
