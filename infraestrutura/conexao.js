810cf6bad2bd3c381ff3ae17bc008a3f19a9a2de
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-11-23 00:26:35 -0300

    Alterando host

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const conexao = mysql.createPool({
-    host: '198.100.155.70',
+    host: 'localhost',
     port: '3306',
     user: 'vegeta',
     password: 'uNC5fDdUpjaM7qqA',
